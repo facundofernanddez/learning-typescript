6e26973a0daa9d980f120646c465cd606313cde6
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-05-18 14:32:38 -0300

    don't use the type any

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,7 @@ mensaje = "chanchito feliz";
 let edad: number = 26;
 let deporteFavorito: string = "crossfit";
 let adulto: boolean = true;
+
+const chanchitoFeliz = (config: any) => {
+  return config;
+};
